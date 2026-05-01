2d44b9347792a56047d97aa10e5aaa3a54b71608
prathamshahi1 <prathamm0001@gmail.com>
2026-05-01 18:02:48 +0530

    update api url for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://responsible-simplicity-production.up.railway.app/api"
 });
 
 // 🔥 THIS IS CRITICAL
