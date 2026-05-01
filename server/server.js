77a3633577531bb78b7db7dda7ed5bde1f23875f
prathamshahi1 <prathamm0001@gmail.com>
2026-05-01 19:38:57 +0530

    fix wildcard crash

--- server/server.js
+++ server/server.js
@@ -49,7 +49,7 @@ const frontendPath = path.join(__dirname, "dist");
 app.use(express.static(frontendPath));
 
 // ✅ IMPORTANT: catch-all LAST
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
 
